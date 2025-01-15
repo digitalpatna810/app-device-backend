b5e918a88a2a3a0cd68a60a8bbca3418b0608f69
kishan <kishan@vidyainternahub.com>
2025-01-16 00:34:17 +0530

    added swagger

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: "device-manager-backend",
-        script: "/src/app.ts", // Point to the TypeScript file
+        script: "./src/app.ts", // Point to the TypeScript file
         interpreter: "ts-node", // Use ts-node to execute TypeScript
       },
     ],
