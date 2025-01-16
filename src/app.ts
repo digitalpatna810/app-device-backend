7497504c7fa3501712dd944c6c019727108ff8ab
kishan <kishan@vidyainternahub.com>
2025-01-16 22:43:45 +0530

    cors added

--- src/app.ts
+++ src/app.ts
@@ -25,6 +25,17 @@ app.use("/api", notificationRoutes);
 
 swaggerDocs(app);
 
+app.use(cors({   origin: '*', 
+methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],                                                              
+allowedHeaders: ['Content-Type', 'Authorization'], }));
+
+
+app.use((req, res, next) => {  
+   res.header("Access-Control-Allow-Origin", "*");   
+   res.header("Access-Control-Allow-Methods", "GET , PUT , POST , DELETE");  
+ res.header("Access-Control-Allow-Headers", "Content-Type, x-requested-with");   
+ next(); })
+ 
 const port = process.env.PORT || 8000;
 
 connectDB().then(() => {
