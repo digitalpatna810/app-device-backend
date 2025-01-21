9a5555b03000b0d5969d9be359ad16f8049b03cd
kishan <kishan@vidyainternahub.com>
2025-01-21 21:50:51 +0530

    env added for live url

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,33 @@
-module.exports = {
-    apps: [
-      {
-        name: "device-manager-backend",
-        script: "src/app.ts",
-        watch: "true",
-        interpreter: "ts-node", // Use ts-node as the interpreter
-        env: {
-          NODE_ENV: "development",
-        },
-        env_production: {
-          NODE_ENV: "production",
-        },
-      },
-    ],
-  };
+// module.exports = {
+//     apps: [
+//       {
+//         name: "device-manager-backend",
+//         script: "src/app.ts",
+//         watch: "true",
+//         interpreter: "ts-node", // Use ts-node as the interpreter
+//         env: {
+//           NODE_ENV: "development",
+//         },
+//         env_production: {
+//           NODE_ENV: "production",
+//         },
+//       },
+//     ],
+//   };
   
+module.exports = {
+  apps: [
+    {
+      name: "device-manager-backend",
+      script: "src/app.ts",
+      watch: true,  // Corrected the watch field to a boolean
+      interpreter: "node_modules/.bin/ts-node", // Point to ts-node executable in node_modules
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
