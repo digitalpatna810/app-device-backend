0b8887f27aca82de4485fc29b0695fabecb6ce72
kishan <kishan@vidyainternahub.com>
2025-02-07 23:19:00 +0530

    added admin profile

--- src/routes/contacts-route.ts
+++ src/routes/contacts-route.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post("/create-contact", verifyJWT, saveContact);
 router.get("/contact-details/:contactId", verifyJWT, getContactById);
-router.get("/allContacts", verifyJWT,authorizeAdmin, allContacts);
+// router.get("/allContacts", verifyJWT,authorizeAdmin, allContacts);
 router.get("/allContacts", allContacts)
 router.put("/update-contact/:contactId", verifyJWT, updateContact);
 router.delete("/delete-contact/:contactId", verifyJWT, deleteContact);
