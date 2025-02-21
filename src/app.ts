308f9e712f56db18e374a41843be3ca1387cea92
moksh <moksh.c@cisinlabs.com>
2025-02-21 14:36:45 +0530

    Frontend build

--- src/app.ts
+++ src/app.ts
@@ -10,13 +10,16 @@ import locationRoutes from "./routes/location-route"
 import cors from "cors";
 import connectDB from "./config/mongodb";
 import swaggerDocs from './config/swagger';
+import path from "path";
 
 const app : Express = express();
 
 dotenv.config();
 app.use(express.json());
 app.use(cors());
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
 app.use("/api", profileRoutes);
 app.use("/api", authRoutes);
 app.use("/api", adminRoutes);
