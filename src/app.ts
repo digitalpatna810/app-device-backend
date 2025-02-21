ad47d36a71e7b008fb5bcf8c54c799fb6fe87e4b
moksh <moksh.c@cisinlabs.com>
2025-02-21 14:44:39 +0530

    Frontend build

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,6 @@ import cors from "cors";
 import connectDB from "./config/mongodb";
 import swaggerDocs from './config/swagger';
 import path from "path";
-import mime from "mime-types";
 
 const app : Express = express();
 
@@ -19,13 +18,7 @@ dotenv.config();
 app.use(express.json());
 app.use(cors());
 
-
-app.use((req, res, next) => {
-  if (req.path.endsWith(".js")) {
-    res.type(mime.lookup(req.path) || "application/javascript");
-  }
-  next();
-});
+app.use(express.static(path.join(__dirname, "../build")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../build", "index.html"));
